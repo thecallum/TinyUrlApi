67248d43834be250c4f4e5db60484e0e3df23785
thecallum <callummac@protonmail.com>
2025-10-19 18:40:00 +0100

    Cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,15 @@ set -e
 
 cd /app/TinyUrl
 
-echo "⏳ Waiting for postgres..."
+echo "Waiting for postgres..."
 until dotnet ef database update; do
   echo "   Postgres not ready or migrations failed, retrying..."
   sleep 3
 done
 
-echo "✅ Postgres is ready and migrations completed!"
+echo "Migrations completed!"
 
-echo "🎯 Starting application..."
+echo "Starting application..."
 echo "==================================="
 
 cd /app/build
